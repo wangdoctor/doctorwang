ad64de9aa4f86b0aa1e1760d721fa7a71290c223
wb_wangzhe11 <wb_wangzhe11@meituan.com>
2024-03-15 17:56:33 +0800

    save

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,10 @@ const repo = 'doctorwang'; // 项目名
 
 export default defineConfig({
   title: 'Doctor',
-
+  themeConfig:{
+    logo:'/logo.png',
+    name:'Doctor',
+  },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
