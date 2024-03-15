fccdc84184f4e676b1e10d7d6d5c83b5766589b6
wb_wangzhe11 <wb_wangzhe11@meituan.com>
2024-03-15 17:22:58 +0800

    save

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,10 +4,6 @@ const repo = 'doctor docs'; // 项目名
 
 export default defineConfig({
   title: 'doctor docs',
-  mode: 'site',
-  devServer: {
-    port: 1992, // 自定义端口号
-  },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
