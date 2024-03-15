f369e6b0c193cd6c6313ab0e35e51ae169932b8b
wb_wangzhe11 <wb_wangzhe11@meituan.com>
2024-03-15 16:57:21 +0800

    init

---
+++ .dumirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'dumi';
+
+const repo = 'doctor docs'; // 项目名
+
+export default defineConfig({
+  title: 'doctor docs',
+  mode: 'site',
+  devServer: {
+    port: 1992, // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+
+  // more config: https://d.umijs.org/config
+});
